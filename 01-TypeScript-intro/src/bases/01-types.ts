b694d719a39c537a476026ecd839162a7c72c227
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-11-19 09:57:54 -0400

    [V.0.0.4] Pruebas de tipados en typescript

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -8,3 +8,27 @@ console.log('[name]: ', name);
 export const age: number = 20;
 
 console.log('[age]: ', age);
+
+// Aquí podemos ver un error muy común
+// Declaramos una variable tipo string o nùmero o undefined
+let error: string | number | undefined
+
+// Pero si intentamos asignarle un valor booleano directamente nos mandará un error
+
+//error = true;
+error = ''
+
+// Beneficios del typescript, para ver errores en el còdigo antes de compilarlo
+
+// Tambien se pueden exportar templateStrings
+
+export const templateString = `Esto es un string 
+multilinea
+'' se pueden agregar comillas simples
+"" o dobles
+inyectar valores ${name}
+numeros ${age}
+entre otras cosas
+`
+
+console.log('[templateString]: ', templateString)
