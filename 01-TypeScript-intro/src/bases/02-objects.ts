a45b3b40a5078400a5e2ae3fd3348eb384c4be13
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-11-19 11:04:40 -0400

    [V.0.0.5] Creando interfaces y objetos

---
+++ 01-TypeScript-intro/src/bases/02-objects.ts
@@ -0,0 +1,29 @@
+// Podemos tambien tipar los objetos
+export const pokemonIds = [0,1,2,3,4]
+// Por defecto este objeto sera de numeros
+
+// Por ende, este comando dara error al intentar insertar un string
+//pokemonIds.push('string')
+// Pero igualmente se puede agregar strings aunque de error en el còdigo
+
+
+// Tambien se pueden crear las interfaces, por asi decirlo las templates
+interface Pokemon {
+    id:number;
+    name:string;
+    age?:number; // Si le agregamos un ? al final puede tener o no puede tener este valor
+}
+
+export const pikachu:Pokemon = {
+    id:1,
+    name:'Pikachu'
+}
+
+// Si no rellenamos los campos de la interfaz Pokemon nos dara un error, o si lo agregamos con un valor inadecuado tambien
+// Aqui solo agregamos el nombre y nos da error, porque nos falta el id
+
+// export const charmander: Pokemon ={
+//     name:'Charmander';
+// }
+
+console.log('[Pikachu]: ', pikachu)
--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -3,20 +3,14 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 import { age, name } from './bases/01-types.ts'
+import { pikachu, pokemonIds } from './bases/02-objects.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
     <h1>¡Hola ${name}!</h1>
     <h2>Tengo ${age} años de edad</h2>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
+    <p>${pokemonIds.join(' , ')}!!</p>
+    <p>El pokemon elegido es: <b>${pikachu.name}</b></p>
   </div>
 `
 
