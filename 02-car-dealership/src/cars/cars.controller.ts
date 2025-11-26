2b34fbe33f2d309ab3304d9313f90984f7654fdb
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-11-26 14:35:46 -0400

    [V0.1.5] feat: Creacion del primer servicio y creada la logica para la obtencion de los carros completos y obtencion de un unico carro (mismos endpoints)

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,18 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
-
-
-
+import { CarsService } from './cars.service';
 @Controller('cars')
     export class CarsController {
-        private cars = ['Toyota', 'Honda', 'Jeep']
+
+        constructor(
+            private readonly carsService: CarsService
+        ){}
+        
         @Get()
         getAllCars(){
-        return this.cars
+        return this.carsService.findAll()
     }
 
     @Get(':id')
-    getCarById( @Param ('id') id){
+    getCarById( @Param ('id') id:string){
         console.log('getCarById:',{id: +id})
-        return this.cars[id];
+        return this.carsService.findOneById( Number(id));
     }
 }
